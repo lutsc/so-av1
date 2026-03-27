7c4e9c04d6d04e49ef59b4d0d636ecafa2f95db5
lutsc <170535336+lutsc@users.noreply.github.com>
2026-03-27 12:20:51 -0300

    Common structs and functions added, .vscode/ added to .gitignore

--- include/library.h
+++ include/library.h
@@ -1,6 +1,36 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+/* ===== Constants ===== */ 
+#define MODE_NEG 0
+#define MODE_SLICE 1 
+
+struct PGM {
+  int w, h, maxv; // maxv = 255 
+  unsigned char* data; // w*h bytes (grayscale)
+}; 
+
+struct Header {
+  int w, h, maxv; // image metadata
+  int mode; // 0=NEGATIVE, 1=SLICE 
+  int t1, t2; // valid if mode=SLICE 
+};
+
+struct Task { 
+  int row_start; 
+  int row_end; 
+};
+
+/* ===== I/O of PGM ===== */ 
+
+// int read_pgm(const char* path, PGM* img); 
+
+// int write_pgm(const char* path, const PGM* img); 
+
+/* ===== FIFO ===== */ 
+
+const char* FIFO_PATH = "/tmp/imgpipe";
+
 void test_print_function();
 
 #endif
