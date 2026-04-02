1f4f50a9ce5a2a21b2c43b28a22c1f9115b7fda6
Aim <misiakmelkior@gmail.com>
2026-04-02 20:06:32 -0300

    Created branch for threads4

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main()
   read_pgm("input.pgm", &pgm);
 
   PGM neg = pgm_negative(pgm);
-  PGM slice = pgm_slice(pgm, 100, 128);
+  PGM slice = pgm_slice(pgm, 100, 100);
 
   write_pgm("output_slice.pgm", &slice);
   write_pgm("output_neg.pgm", &neg);
